9ba635d6f496c98be0de3aa2fd65364a8217ba3a
Mark Tamis <mark.tamis@touchflows.com>
2018-04-21 21:28:12 +0200

    Bicycling Layer

--- src/components/bicycling-layer.js
+++ src/components/bicycling-layer.js
@@ -1,4 +1,4 @@
 import createEntity from './entity';
-import BicyclingLayerEvents from '../events/transit-layer';
+import BicyclingLayerEvents from '../events/bicycling-layer';
 
 export default createEntity('BicyclingLayer', null, BicyclingLayerEvents);
